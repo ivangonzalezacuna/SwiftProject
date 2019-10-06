788f50dd66cc466fd24b2b41c8c4dab7ecbfd14a
Iván González Acuña <49096359+ivangonzalezacuna@users.noreply.github.com>
2019-10-06 19:30:44 +0300

    Update ViewController.swift

--- Data Transfer - L4/Data Transfer - L4/ViewController.swift
+++ Data Transfer - L4/Data Transfer - L4/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textField: UITextField! // Definition of our TextField
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,34 +18,34 @@ class ViewController: UIViewController {
     }
     
     @IBAction func button1Clicked(_ sender: Any) {
-        let text = textField.text!
+        let text = textField.text! // Taking the text from the TextField when we click the Button
         print(text)
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) { // Function to transfer data from one class to other
         if segue.destination is ViewController2 {
-            let vc = segue.destination as? ViewController2
-            vc?.textFromView = "Greetings " + textField.text! + "!"
+            let vc = segue.destination as? ViewController2 // Defines the var 'vc' for the class ViewController2
+            vc?.textFromView = "Greetings " + textField.text! + "!" // We update the var 'textFromView', which is from the other class
         }
     }
 }
 
 class ViewController2: UIViewController {
     
-    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var img: UIImageView! // Define out ImageView
     var textFromView:String = ""
     
-    @IBOutlet weak var labelText: UILabel!
+    @IBOutlet weak var labelText: UILabel! // Define our Label
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let veikko = "Veikko"
         // Do any additional setup after loading the view.
-        labelText?.text = textFromView
-        if (textFromView.isEqual("Greetings " + veikko + "!")) {
+        labelText?.text = textFromView // Set the text we are going to show in the Label
+        if (textFromView.isEqual("Greetings " + veikko + "!")) { // Check if the text received is equals to "Veikko"
             print("Hey")
-            self.img.image = UIImage(named: "sun.jpg")
+            self.img.image = UIImage(named: "sun.jpg") // If yes, we will show a sun image in our ImageView
         }
     }
 }
