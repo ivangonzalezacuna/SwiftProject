bc74d62ee96b8395b366935a0cb57276aa2dc46c
basiljung <54670723+basiljung@users.noreply.github.com>
2019-10-06 15:17:50 +0300

    Update ViewController.swift

--- First lectue/First lectue/ViewController.swift
+++ First lectue/First lectue/ViewController.swift
@@ -8,25 +8,25 @@
 
 import UIKit
 
-internal class ViewController : UIViewController {
-    var text: String = ""
+internal class ViewController : UIViewController { // the class ViewController inheritance UIViewController
+    var text: String = ""  // defines a String Variable with name text
     
-    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textField: UITextField! // initialises the UITextfield with the name textField
     
-    @IBOutlet weak var labelWord: UILabel!
+    @IBOutlet weak var labelWord: UILabel!  // initialises the UILabel  with the name labelWord
     
-    override internal func viewDidLoad(){
+    override internal func viewDidLoad(){ // same as onCreate in android
         langInfo()
     }
     
-    @IBAction func button(_ sender: Any) {
-        print("Pressed")
-        text = textField.text!
-        labelWord.text = "Hello " + text
+    @IBAction func button(_ sender: Any) { // defines the action from the button
+        print("Pressed")                    // prints to console
+        text = textField.text!              // gets the text out of the textfield 
+        labelWord.text = "Hello " + text     // set the text on the label
     }
     
     
-    internal func langInfo(){
+    internal func langInfo(){               
         print("Hello World")
         print(text)
     }
