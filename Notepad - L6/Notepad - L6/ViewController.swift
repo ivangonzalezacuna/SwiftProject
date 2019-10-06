7919775d5df4314564721b60847c31565038980d
basiljung <54670723+basiljung@users.noreply.github.com>
2019-10-06 18:47:21 +0300

    Update ViewController.swift

--- Notepad - L6/Notepad - L6/ViewController.swift
+++ Notepad - L6/Notepad - L6/ViewController.swift
@@ -15,12 +15,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let note = UserDefaults.standard.string(forKey: "note")
-        textField.text = note
+        let note = UserDefaults.standard.string(forKey: "note") // take the text from the text which was in the scree before closing
+        textField.text = note // put it to the textfield
     }
     
     
-    @IBAction func updateUserDefault(_ sender: Any) {
+    @IBAction func updateUserDefault(_ sender: Any) { // evertime the text changes it updates the userdefault value for key:note
     UserDefaults.standard.set(textField.text!, forKey: "note")
     }
 }
